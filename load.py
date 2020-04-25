e1bdad558ab2636792c1b8ec0e31c9404b837631
AdmiralGallade <mailforkushagra2@gmail.com>
2020-04-25 06:54:11 -0400

    Minor changes

--- load.py
+++ load.py
@@ -2,15 +2,16 @@ from __future__ import print_function
 import argparse
 import cv2
 
+#usage example: python .\load.py --image .\newimage.jpg
 
 ap=argparse.ArgumentParser()
 ap.add_argument("-i","--image",required=True,help="Image path")
 args=vars(ap.parse_args())
 
 image= cv2.imread(args["image"])
-print("width: [] pixels".format(image.shape[1]))
-print("height: [] pixels".format(image.shape[0]))
-print("channels: [] pixels".format(image.shape[2]))
+print("width: {} pixels".format(image.shape[1]))
+print("height: {} pixels".format(image.shape[0]))
+print("channels: {} pixels".format(image.shape[2]))
 
 cv2.imshow("Image",image)
 cv2.waitKey(0)
